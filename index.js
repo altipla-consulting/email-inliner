c84838e953396478c7500f2addb4affb1bf696aa
Ernesto Alejo Oltra <ernestokarim@gmail.com>
2015-01-16 22:38:19 +0100

    Parse JSON input to read instructions of the app.

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var juice = require('juice'),
-    argv = require('minimist')(process.argv.slice(2)),
     fs = require('fs'),
     _ = require('lodash'),
     path = require('path');
@@ -17,61 +16,66 @@ goog.require('goog.i18n.DateTimeSymbols_en');
 goog.require('goog.i18n.DateTimeSymbols_fr');
 
 
-if (!argv.file) {
-  throw new Error('a --file argument is needed');
-}
-if (argv.datetimes && !argv.lang) {
-  throw new Error('--lang is required if you are using datetimes');
-}
-
-
-var datetimes = [];
-if (argv.datetimes) {
-  datetimes = JSON.parse(argv.datetimes);
-
-  switch (argv.lang) {
-    case 'es':
-      goog.i18n.DateTimeSymbols = goog.i18n.DateTimeSymbols_es;
-      goog.i18n.DateTimePatterns = goog.i18n.DateTimePatterns_es;
-      break;
-
-    case 'en':
-      goog.i18n.DateTimeSymbols = goog.i18n.DateTimeSymbols_en;
-      goog.i18n.DateTimePatterns = goog.i18n.DateTimePatterns_en;
-      break;
-
-    case 'fr':
-      goog.i18n.DateTimeSymbols = goog.i18n.DateTimeSymbols_fr;
-      goog.i18n.DateTimePatterns = goog.i18n.DateTimePatterns_fr;
-      break;
-
-    default:
-      throw new Error('unrecognized lang: ' + argv.lang);
-  }
-}
-
-
-var contents = fs.readFileSync(argv.file).toString();
-_.each(datetimes, function (datetime) {
-  datetime.name = datetime.name.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&');
-  datetime.name = datetime.name.toUpperCase();
-  var variable = new RegExp('\\[\\[\\$' + datetime.name + '\\]\\]', 'g');
-
-  var formatter = new goog.i18n.DateTimeFormat(datetime.format);
-  var date = goog.date.DateTime.fromTimestamp(datetime.value * 1000);
-  var output = formatter.format(date);
-
-  contents = contents.replace(variable, output);
+ 
+var input = [];
+process.stdin.setEncoding('utf8'); 
+process.stdin.on('data', function (chunk) {
+    input.push(chunk);
 });
 
 
-var options = {
-  url: 'file:///' + path.resolve(process.cwd(), argv.file),
-};
-juice.juiceContent(contents, options, function (err, html) {
-  if (err) {
-    throw err;
+process.stdin.on('end', function () {
+  input = JSON.parse(input);
+
+  if (input.datetimes && !input.lang) {
+    throw new Error('lang is required if you are using datetimes');
   }
 
-  console.log(html);
+  if (input.datetimes) {
+    switch (input.lang) {
+      case 'es':
+        goog.i18n.DateTimeSymbols = goog.i18n.DateTimeSymbols_es;
+        goog.i18n.DateTimePatterns = goog.i18n.DateTimePatterns_es;
+        break;
+
+      case 'en':
+        goog.i18n.DateTimeSymbols = goog.i18n.DateTimeSymbols_en;
+        goog.i18n.DateTimePatterns = goog.i18n.DateTimePatterns_en;
+        break;
+
+      case 'fr':
+        goog.i18n.DateTimeSymbols = goog.i18n.DateTimeSymbols_fr;
+        goog.i18n.DateTimePatterns = goog.i18n.DateTimePatterns_fr;
+        break;
+
+      default:
+        throw new Error('unrecognized lang: ' + input.lang);
+    }
+  }
+
+  _.each(input.datetimes, function (datetime) {
+    datetime.name = datetime.name.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&');
+    datetime.name = datetime.name.toUpperCase();
+    var variable = new RegExp('\\[\\[\\$' + datetime.name + '\\]\\]', 'g');
+
+    var formatter = new goog.i18n.DateTimeFormat(datetime.format);
+    var date = goog.date.DateTime.fromTimestamp(datetime.value * 1000);
+    var output = formatter.format(date);
+
+    input.contents = input.contents.replace(variable, output);
+  });
+
+  var options = {
+    url: 'file://' + path.resolve(process.cwd(), input.cwd),
+  };
+  console.log(options);
+  juice.juiceContent(input.contents, options, function (err, html) {
+    if (err) {
+      throw err;
+    }
+
+    console.log(html);
+  });
 });
+
+
